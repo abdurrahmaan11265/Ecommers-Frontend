c2c273626704745f0505c222e702f6ec074edbd3
Mohammed Abdur Rahmaan <112614695+abdurrahmaan11265@users.noreply.github.com>
2024-06-11 11:32:05 +0530

    add netlify file

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -138,9 +138,9 @@ const OurStore = () => {
                 <div className="d-flex justify-content-between align-items-center">
                   <div className="d-flex align-items-center gap-10">
                     <p className="mb-0 d-block" style={{ width: "100px" }}>Sort By:</p>
-                    <select name="" className='form-control form-select' id="">
+                    <select name="" className='form-control form-select' id="" defaultValue='best-selling'>
                       <option value="manual">Featured</option>
-                      <option value="best-selling" selected='selected'>Best Selling</option>
+                      <option value="best-selling">Best Selling</option>
                       <option value="title-ascending">Alphabetically, A-Z</option>
                       <option value="title-descending">Alphabetically, Z-A</option>
                       <option value="price-ascending">Price, Low to High</option>
